7ad59078c7c5985d3be56accaa21c2960b211c9f
jfkbeatkid <hernadezrigoberto@gmail.com>
2018-11-15 13:30:24 -0800

    messed with jsx.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+// Import the React and ReactDOM Libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create a react component
+const App = () => {
+  const buttonText = "Click me!";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  return (
+    <div>
+      <label className="label" for="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{backgroundColor: 'blue', color: 'white'}}>
+        {buttonText}
+      </button>
+    </div>
+  );
+}
+
+// Take the react component and show it on the screen
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
